b378b88cd5ef7cec8b0eef28b131fcd8bdfe1214
vova <vk4935391@gmail.com>
2026-09-10 10:41:50 +0300

    feat: add basic timer implementation with a GUI

--- Timer/src/main/java/volodea/Main.java
+++ Timer/src/main/java/volodea/Main.java
@@ -1,5 +1,10 @@
 package volodea;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 class consoleTimerVane {
 
 }
@@ -7,10 +12,62 @@ class consoleTimerVane {
 
 public class Main {
     static void main() {
-
+        GuiTimerVova guiTimerVova = new GuiTimerVova();
+        guiTimerVova.startApp();
     }
 }
 
 class GuiTimerVova{
+    JFrame frame = new JFrame("First lab - GUI interface");
 
+    JProgressBar progressBar = new JProgressBar(0, 100);
+    JLabel progressLabel = new JLabel("Статус: Ожидание");
+    JButton startButton = new JButton("Start");
+
+    ActionListener actionListener = new ActionListener() {
+        int currProgress = 0;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            currProgress += 1;
+            progressBar.setValue(currProgress);
+
+            if(currProgress >= 100){
+                ((Timer) e.getSource()).stop();
+                progressLabel.setText("Статус: Завершён");
+                startButton.setEnabled(true);
+            }
+        }
+    };
+
+    Timer timer = new Timer(100, actionListener);
+
+    ActionListener actionListenerStarter = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            progressLabel.setText("Статус: Выполнение...");
+            startButton.setEnabled(false);
+            timer.start();
+        }
+    };
+
+    GuiTimerVova() {
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+    }
+
+    private void setComponentsOnWindow() {
+        frame.add(progressBar);
+        frame.add(progressLabel);
+        frame.add(startButton);
+    }
+
+    public void startApp() {
+        setComponentsOnWindow();
+
+        startButton.addActionListener(actionListenerStarter);
+
+        frame.setVisible(true);
+    }
 }
